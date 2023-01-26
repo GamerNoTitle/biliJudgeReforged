600bcb8be7070e0c91ce8aaa97e3e302116c3bbd
GamerNoTitle <GamerNoTitle@outlook.com>
2023-01-26 18:25:54 +0800

    Use logger to debug

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,15 +2,23 @@ import os
 from utils.action import convert_cookies_to_dict, user
 from dotenv import load_dotenv, dotenv_values
 import utils.const as const
+from utils.logger import logger as log
 load_dotenv()
+loglevel = dotenv_values()['LOGLEVEL']
+if dotenv_values()['SAVELOG'] == '1':
+    logger = log(log_level = loglevel, log_file='info.log')
+    logger.status = 1
+else:
+    logger = log(log_level = loglevel)
+    logger.status = 0
 SESSDATA, CSRF = dotenv_values()['SESSDATA'], dotenv_values()['CSRF']
 if SESSDATA == '' or CSRF == '':
-    print('[ERROR] You must configure SESSDATA and CSRF to use this script!')
+    logger.error('[ERROR] You must configure SESSDATA and CSRF to use this script!')
     os._exit(-1)
 UA = dotenv_values()['UA']
 if UA == '':
     UA = const.UA
-    print(f'[WARN] It seems that you haven\'t configurate the UA, use fallback UA [{UA}] instead.')
+    logger.warn(f'[WARN] It seems that you haven\'t configurate the UA, use fallback UA [{UA}] instead.')
 
-my = user(SESSDATA, CSRF, UA)
+my = user(SESSDATA, CSRF, UA, logger)
 my.applyFor()
--- utils/action.py
+++ utils/action.py
@@ -1,14 +1,16 @@
 import requests, json, os
+from logging import Logger
 from utils.biliAPI import base_info, qualification, apply
 def convert_cookies_to_dict(cookie):
     cookie = dict([l.split("=", 1) for l in cookie.split("; ")])
     return cookie
 
 class user:
-    def __init__(self, SESSDATA, CSRF, UA):
+    def __init__(self, SESSDATA, CSRF, UA, logger: Logger):
         self.SESSDATA = SESSDATA
         self.CSRF = CSRF
         self.UA = UA
+        self.logger = logger
 
     def getBaseInfo(self):
         headers = {
@@ -26,6 +28,7 @@ class user:
             self.allow_apply = data['data']['allow_apply']
             self.apply_status = data['data']['apply_status']
             self.status = data['data']['status']
+        return response.text
 
     def getQualification(self):
         headers = {
@@ -42,11 +45,14 @@ class user:
             self.blocked = data['data']['blocked']
             self.cert = data['data']['cert']
             self.rule = data['data']['rule']
+        return response.text
 
     def applyFor(self):
-        self.getBaseInfo()
-        self.getQualification()
-        # print(self.login, self.blocked, self.cert, self.rule, self.login, self.allow_apply, self.apply_status, self.status)
+        baseinfo = self.getBaseInfo()
+        qualifi = self.getQualification()
+        if self.logger.status:
+            self.logger.info(baseinfo)
+            self.logger.info(qualifi)
         if self.login:
             if not self.blocked and self.cert and self.rule and self.allow_apply and self.apply_status != 5:
                 # 登录，未被小黑屋，有实名，90天内无封禁，允许申请且此周期内没有申请过
@@ -58,28 +64,30 @@ class user:
                 params = {'csrf': self.CSRF}
                 response = requests.post(apply, params=params, headers=headers)
                 data = json.loads(response.text)
+                if self.logger.status:
+                    self.logger.info(response.text)
                 if data['code'] == 0:
-                    print('[INFO] Welcome to JUDGEMENT!')
+                    self.logger.info('[INFO] Your application has been submitted. Please wait for ')
                 elif data['code'] == -101:
-                    print('[ERR] You are not logged in!')
+                    self.logger.error('[ERR] You are not logged in!')
                 elif data['code'] == -111:
-                    print('[ERR] Invalid CSRF token (bili_jct in your cookie)!')
+                    self.logger.error('[ERR] Invalid CSRF token (bili_jct in your cookie)!')
                 elif data['code'] == 25001:
-                    print('[ERR] Your level is TOO low!')   # Previous version
+                    self.logger.error('[ERR] Your level is TOO low!')   # Previous version
                 elif data['code'] == 25002:
-                    print('[ERR] You are not surfing with your real name!')
+                    self.logger.error('[ERR] You are not surfing with your real name!')
                 elif data['code'] == 25003:
-                    print('[ERR] You have banned log in previous 90 days!')
+                    self.logger.error('[ERR] You have banned log in previous 90 days!')
                 elif data['code'] == 25013:
-                    print('[ERR] You can apply for qualification while you are already have it!')
+                    self.logger.error('[ERR] You can apply for qualification while you are already have done it. Please wait for pass!')
                 elif data['code'] == 25016:
-                    print('[ERR] No more qualification today, please retry tomorrow!')
+                    self.logger.error('[ERR] No more qualification today, please retry tomorrow!')
                 if data['code'] != 0:
-                    print(response.text)
+                    # print(response.text)
                     os._exit(-1)
             else:
-                print('[ERR] You are not qualified to JUDGEMENT!')
+                self.logger.error('[ERR] You are not qualified to JUDGEMENT!')
                 os._exit(-1)
         else:
-            print('[ERR] You are not logged in!')
+            self.logger.error('[ERR] You are not logged in!')
             os._exit(-1)
